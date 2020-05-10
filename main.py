5fc59ca2fb12ee789935417f1aad7e12dcf0c50f
Scott Kemperman <skemperman@ascendentgroup.com>
2020-05-10 12:30:26 -0600

    doing stuff

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ BANNER = """
 -------------------------------------------------------------
 """ % datetime.now().strftime(DATETIME_FORMAT)
 
+
 def view_exercises():
     with open('data/exercises.json', 'r') as file:
         exercises = json.load(file)
@@ -28,6 +29,7 @@ def view_exercises():
     print(json.dumps(exercises, indent=4))
 
     input('\npress enter to return to menu...')
+
 
 def view_muscle_groups():
     with open('data/muscleGroups.json', 'r') as file:
@@ -39,14 +41,18 @@ def view_muscle_groups():
 
     input('\npress enter to return to menu...')
 
+
 """ STATES = YEAR_SELECT, MONTH_SELECT, DAY_SELECT, HOUR_SELECT, MINUTE_SELECT, CHOICES_SELECT """
+
+
 def create_session():
     with open('data/exercises.json', 'r') as file:
         exercises = json.load(file)
 
     choices = []
     date = (datetime.now() + timedelta(days=1)).replace(minute=0)
-    states = ['YEAR_SELECT', 'MONTH_SELECT', 'DAY_SELECT', 'HOUR_SELECT', 'MINUTE_SELECT', 'CHOICES_SELECT', 'FINISHED']
+    states = ['YEAR_SELECT', 'MONTH_SELECT', 'DAY_SELECT',
+              'HOUR_SELECT', 'MINUTE_SELECT', 'CHOICES_SELECT', 'FINISHED']
     stateid = 0
 
     while True:
@@ -63,7 +69,8 @@ def create_session():
 
         click.echo()
         if state == 'YEAR_SELECT':
-            click.echo('\tSelect Year (ENTER=%d | ARROWS): ' % date.year, nl=False)
+            click.echo('\tSelect Year (ENTER=%d | ARROWS): ' %
+                       date.year, nl=False)
             recv = click.getchar().encode('utf-8')
             if recv == UTF_DOWN or recv == UTF_LEFT:
                 date = date.replace(year=date.year - 1)
@@ -73,7 +80,8 @@ def create_session():
                 stateid += 1
 
         elif state == 'MONTH_SELECT':
-            click.echo('\tSelect Month (ENTER=%s | ARROWS): ' % date.strftime("%b"), nl=False)
+            click.echo('\tSelect Month (ENTER=%s | ARROWS): ' %
+                       date.strftime("%b"), nl=False)
             recv = click.getchar().encode('utf-8')
             if recv == UTF_DOWN or recv == UTF_LEFT:
                 date = utils.add_months(date, -1)
@@ -83,7 +91,8 @@ def create_session():
                 stateid += 1
 
         elif state == 'DAY_SELECT':
-            click.echo('\tSelect Day (ENTER=%d | ARROWS): ' % date.day, nl=False)
+            click.echo('\tSelect Day (ENTER=%d | ARROWS): ' %
+                       date.day, nl=False)
             recv = click.getchar().encode('utf-8')
             if recv == UTF_DOWN or recv == UTF_LEFT:
                 date = utils.add_days(date, -1)
@@ -93,7 +102,8 @@ def create_session():
                 stateid += 1
 
         elif state == 'HOUR_SELECT':
-            click.echo('\tSelect Hour (ENTER=%d | ARROWS): ' % date.hour, nl=False)
+            click.echo('\tSelect Hour (ENTER=%d | ARROWS): ' %
+                       date.hour, nl=False)
             recv = click.getchar().encode('utf-8')
             if recv == UTF_DOWN or recv == UTF_LEFT:
                 date = date - timedelta(hours=1)
@@ -103,7 +113,8 @@ def create_session():
                 stateid += 1
 
         elif state == 'MINUTE_SELECT':
-            click.echo('\tSelect Minute (ENTER=%d | ARROWS): ' % date.minute, nl=False)
+            click.echo('\tSelect Minute (ENTER=%d | ARROWS): ' %
+                       date.minute, nl=False)
             recv = click.getchar().encode('utf-8')
             if recv == UTF_DOWN or recv == UTF_LEFT:
                 date = date - timedelta(minutes=15)
@@ -114,8 +125,10 @@ def create_session():
 
         elif state == 'CHOICES_SELECT':
             for i in range(len(exercises)):
-                click.echo('\t\t(%d) - %s' % (i + 1, list(exercises.keys())[i]))
-            click.echo('\n\tSelect Next Exercise (ENTER=FINISHED | #): ', nl=False)
+                click.echo('\t\t(%d) - %s' %
+                           (i + 1, list(exercises.keys())[i]))
+            click.echo(
+                '\n\tSelect Next Exercise (ENTER=FINISHED | #): ', nl=False)
             recv = input()
             if recv.isdigit() and 0 < int(recv) <= len(exercises):
                 choices.append(list(exercises.keys())[int(recv) - 1])
@@ -128,7 +141,8 @@ def create_session():
             with open('data/sessions.json', 'r') as file:
                 sessions = json.load(file)
             with open('data/sessions.json', 'w') as file:
-                sid = hash(frozenset([date.strftime(DATETIME_FORMAT), *choices]))
+                sid = hash(
+                    frozenset([date.strftime(DATETIME_FORMAT), *choices]))
                 sessions.append(dict(
                     id=sid,
                     timeScheduled=date.strftime(DATETIME_FORMAT),
@@ -140,11 +154,15 @@ def create_session():
                 json.dump(sessions, file)
             return
 
+
 """ STATES = SELECT_SESSION, SELECT_EXERCISE, SELECT_REPS, SELECT_WEIGHT"""
+
+
 def enter_session():
     with open('data/sessions.json', 'r') as file:
         sessions = json.load(file)
-        sessions.sort(key=lambda s: datetime.strptime(s['timeScheduled'], DATETIME_FORMAT))
+        sessions.sort(key=lambda s: datetime.strptime(
+            s['timeScheduled'], DATETIME_FORMAT))
 
     with open('data/exercises.json', 'r') as file:
         exercise_data = json.load(file)
@@ -160,22 +178,41 @@ def enter_session():
         click.echo(BANNER)
 
         if active_session:
-            click.echo('\tACTIVE SESSION (%s):' % active_session['timeScheduled'])
+            click.echo('\tACTIVE SESSION (%s):' %
+                       active_session['timeScheduled'])
             click.echo('\tStarted:\t%s' % active_session['timeStarted'])
-            elapsed = datetime.now() - datetime.strptime(active_session['timeStarted'], DATETIME_FULL_FORMAT)
-            click.echo('\tDuration:\t%s:%s' % (int(elapsed.seconds / 60), elapsed.seconds % 60))
+            elapsed = datetime.now() - \
+                datetime.strptime(
+                    active_session['timeStarted'], DATETIME_FULL_FORMAT)
+            click.echo('\tDuration:\t%s:%s\n' %
+                       (int(elapsed.seconds / 60), elapsed.seconds % 60))
+
+            complete_exercises = [
+                complete_ex for complete_ex in active_session['completions']]
+            exercises = active_session['exercises']
+            for i in range(len(exercises)):
+                weight_suggestion = [12, 15, 17]
+                click.echo('\t\t(%d) - %s \t%s \t%s' % (i + 1,
+                                                   exercises[i],
+                                                   weight_suggestion,
+                                                   '*COMPLETE*' if exercises[i] in complete_exercises else ''))
+
             for exercise in active_session['completions']:
-                click.echo('\n\tExercise:\t%s (%d)' % (exercise, len(active_session['completions'][exercise])))
+                click.echo('\n\tExercise:\t%s (%d)' % (
+                    exercise, len(active_session['completions'][exercise])))
                 for i in range(len(active_session['completions'][exercise]['sets'])):
                     aset = active_session['completions'][exercise]['sets'][i]
-                    click.echo('\t\tSet %s: %s' % (i + 1, aset['reps']), nl=False)
-                    click.echo('\t\t%s lbs' % aset['weight'] if aset['weight'] else '')
+                    click.echo('\t\tSet %s: %s' %
+                               (i + 1, aset['reps']), nl=False)
+                    click.echo('\t\t%s lbs' %
+                               aset['weight'] if aset['weight'] else '')
 
         click.echo('\n')
 
         if state == 'SELECT_SESSION':
             for i in range(len(sessions)):
-                day_id = datetime.strptime(sessions[i]['timeScheduled'], DATETIME_FORMAT).weekday()
+                day_id = datetime.strptime(
+                    sessions[i]['timeScheduled'], DATETIME_FORMAT).weekday()
                 day_name = calendar.weekheader(9).split()[day_id]
                 click.echo('\t\t%d. - %s %s (%d)' % (i + 1,
                                                      day_name,
@@ -190,19 +227,19 @@ def enter_session():
 
             if matched_session:
                 active_session = matched_session.copy()
-                active_session['timeStarted'] = datetime.now().strftime(DATETIME_FULL_FORMAT)
+                active_session['timeStarted'] = datetime.now().strftime(
+                    DATETIME_FULL_FORMAT)
                 state = 'SELECT_EXERCISE'
 
         elif state == 'SELECT_EXERCISE':
-            complete_exercises = [complete_ex for complete_ex in active_session['completions']]
+            complete_exercises = [
+                complete_ex for complete_ex in active_session['completions']]
             if len(complete_exercises) == len(active_session['exercises']):
                 state = 'FINISHED'
                 continue
 
             exercises = active_session['exercises']
-            for i in range(len(exercises)):
-                click.echo('\t\t(%d) - %s \t%s' % (i + 1, exercises[i], '*COMPLETE*' if exercises[i] in complete_exercises else ''))
-            click.echo('\n\tBegin Exercise (ENTER=(1) | END | #): ', nl=False)
+            click.echo('\tBegin Exercise (ENTER=(1) | END | #): ', nl=False)
             recv = input()
 
             active_exercise = None
@@ -225,10 +262,12 @@ def enter_session():
                 continue
 
         elif state == 'SELECT_WEIGHT':
-            click.echo('\n\tComplete Exercise (ENTER=NEXT | #=Weight): ', nl=False)
+            click.echo(
+                '\n\tComplete Exercise (ENTER=NEXT | #=Weight): ', nl=False)
             recv = input()
             if recv.isdigit() and 0 < int(recv) <= 100:
-                active_session['completions'][active_exercise]['sets'].append(dict(reps=None, weight=int(recv)))
+                active_session['completions'][active_exercise]['sets'].append(
+                    dict(reps=None, weight=int(recv)))
                 state = 'SELECT_REPS'
             elif recv == '':
                 active_session['completions'][active_exercise]['timeFinished'] = datetime.now().strftime(
@@ -237,13 +276,15 @@ def enter_session():
                 state = 'SELECT_EXERCISE'
 
         elif state == 'SELECT_REPS':
-            click.echo("\n\tComplete Exercise (ENTER=NEXT | #=REPS): ", nl=False)
+            click.echo(
+                "\n\tComplete Exercise (ENTER=NEXT | #=REPS): ", nl=False)
             recv = input()
             if recv.isdigit() and 0 < int(recv) <= 1000:
                 current_set = active_session['completions'][active_exercise]['sets'][-1] if \
                     active_session['completions'][active_exercise]['sets'] else None
                 if not current_set or current_set['reps']:
-                    active_session['completions'][active_exercise]['sets'].append(dict(reps=int(recv), weight=None))
+                    active_session['completions'][active_exercise]['sets'].append(
+                        dict(reps=int(recv), weight=None))
                 else:
                     current_set['reps'] = int(recv)
 
@@ -258,8 +299,10 @@ def enter_session():
                 state = 'SELECT_EXERCISE'
 
         elif state == 'FINISHED':
-            click.pause('\n\t<<< Finalized session completion on %s...' % active_session['timeStarted'])
-            active_session['timeFinished'] = datetime.now().strftime(DATETIME_FULL_FORMAT)
+            click.pause('\n\t<<< Finalized session completion on %s...' %
+                        active_session['timeStarted'])
+            active_session['timeFinished'] = datetime.now().strftime(
+                DATETIME_FULL_FORMAT)
 
             with open('data/sessionHistory.json', 'r') as file:
                 session_history = json.load(file)
@@ -273,11 +316,15 @@ def enter_session():
 
             return
 
+
 """ STATES = LIST_VIEW, SESSION_VIEW """
+
+
 def view_history():
     with open('data/sessionHistory.json', 'r') as file:
         sessions = json.load(file)
-        sessions.sort(key=lambda s: datetime.strptime(s['timeStarted'], DATETIME_FULL_FORMAT))
+        sessions.sort(key=lambda s: datetime.strptime(
+            s['timeStarted'], DATETIME_FULL_FORMAT))
         sessions.reverse()
 
     state = 'LIST_VIEW'
@@ -289,17 +336,21 @@ def view_history():
 
         if state == 'LIST_VIEW':
             for i in range(len(sessions)):
-                start_time = datetime.strptime(sessions[i]['timeStarted'], DATETIME_FULL_FORMAT)
-                end_time = datetime.strptime(sessions[i]['timeFinished'], DATETIME_FULL_FORMAT)
+                start_time = datetime.strptime(
+                    sessions[i]['timeStarted'], DATETIME_FULL_FORMAT)
+                end_time = datetime.strptime(
+                    sessions[i]['timeFinished'], DATETIME_FULL_FORMAT)
                 duration = end_time - start_time
-                day_id = datetime.strptime(sessions[i]['timeStarted'], DATETIME_FULL_FORMAT).weekday()
+                day_id = datetime.strptime(
+                    sessions[i]['timeStarted'], DATETIME_FULL_FORMAT).weekday()
                 day_name = calendar.weekheader(5).split()[day_id].upper()
                 click.echo('\t%d. %s %s (SCHED: %s) Duration: %s (%d)' % (i + 1,
-                                                                         day_name,
-                                                                         sessions[i]['timeStarted'][:-2],
-                                                                         sessions[i]['timeScheduled'],
-                                                                         '%s:%s' % (int(duration.seconds / 60), duration.seconds % 60),
-                                                                         len(sessions[i]['completions'])))
+                                                                          day_name,
+                                                                          sessions[i]['timeStarted'][:-2],
+                                                                          sessions[i]['timeScheduled'],
+                                                                          '%s:%s' % (
+                                                                              int(duration.seconds / 60), duration.seconds % 60),
+                                                                          len(sessions[i]['completions'])))
             click.echo('\n\tView Session (ENTER=MENU | #): ', nl=False)
             recv = input()
             if recv.isdigit() and 0 < int(recv) <= len(sessions):
@@ -315,6 +366,7 @@ def view_history():
             selection = None
             state = 'LIST_VIEW'
 
+
 ACTIONS = {
     "Enter Session": enter_session,
     "Create New Session": create_session,
@@ -322,6 +374,7 @@ ACTIONS = {
     "View Exercises": view_exercises,
     "View Muscle Groups": view_muscle_groups
 }
+
 
 def select_option(options):
     for i in range(len(options)):
@@ -332,6 +385,7 @@ def select_option(options):
     response = input('\tSELECT ACTION (%d - %d): ' % (1, len(options)))
 
     return options[int(response) - 1]
+
 
 class MainApp:
     def __init__(self):
@@ -349,7 +403,6 @@ class MainApp:
                 ACTIONS[response]()
             else:
                 click.echo('\npress enter to return to menu')
-
 
 
 if __name__ == '__main__':
